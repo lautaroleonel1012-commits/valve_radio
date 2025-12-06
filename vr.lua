08f33f6c7451f3cf2fb44f1e72a7a42d69ef3838
lautaroleonel1012-commits <lautaroleonel1012@gmail.com>
2025-12-06 13:30:28 -0300

    Update vr.lua

--- vr.lua
+++ vr.lua
@@ -3,14 +3,11 @@ local UserInputService = game:GetService("UserInputService")
 local lp = Players.LocalPlayer
 local backpack = lp:WaitForChild("Backpack")
 
-
+-- TOOL
 local tool = Instance.new("Tool")
 tool.Name = "Valve radio"
 tool.CanBeDropped = false
 
-
-local handle = Instance.new("Part")
-handle.Name = "Handle"
 local handle = Instance.new("Part")
 handle.Name = "Handle"
 handle.Size = Vector3.new(0.01, 0.01, 0.01)
@@ -25,14 +22,10 @@ mesh.TextureId = "rbxassetid://2255562684"
 mesh.Scale = Vector3.new(0.01, 0.01, 0.01)
 mesh.Parent = handle
 
-
 tool.GripPos = Vector3.new(0.6, -1, 0)
-tool.GripUp = Vector3.new(0, 1, 0)
-tool.GripRight = Vector3.new(0, 0, 0)
-tool.GripForward = Vector3.new(0, 0, 0)
-
 tool.Parent = backpack
 
+-------------------------------------------------
 
 local audioFiles = {
 	"conga_sketch_167bpm_01-04.wav",
@@ -43,82 +36,68 @@ local audioFiles = {
 	"Half-Life10.mp3","Half-Life11.mp3","Half-Life12.mp3","Half-Life13.mp3",
 	"Half-Life14.mp3","Half-Life15.mp3","Half-Life16.mp3","Half-Life17.mp3",
 
-	"hl2_song1.mp3",
-	"hl2_song3.mp3",
-	"hl2_song4.mp3",
-	"hl2_song7.mp3",
-	"hl2_song14.mp3",
-	"hl2_song15.mp3",
-	"hl2_song16.mp3",
-	"hl2_song17.mp3",
-	"hl2_song19.mp3",
-	"hl2_song20_submix0.mp3",
-	"hl2_song20_submix4.mp3",
-	"hl2_song23_suitsong3.mp3",
-	"hl2_song29.mp3",
-	"hl2_song31.mp3",
-	"hl2_song32.mp3",
+	"hl2_song1.mp3","hl2_song3.mp3","hl2_song4.mp3","hl2_song7.mp3",
+	"hl2_song14.mp3","hl2_song15.mp3","hl2_song16.mp3","hl2_song17.mp3",
+	"hl2_song19.mp3","hl2_song20_submix0.mp3","hl2_song20_submix4.mp3",
+	"hl2_song23_suitsong3.mp3","hl2_song29.mp3","hl2_song31.mp3","hl2_song32.mp3",
 
-	"looping_radio_mix.wav",
-	"mannrobics.wav",
-	"portal_4000_degrees_kelvin.mp3",
-	"portal_still_alive.mp3",
-	"portal_you_cant_escape_you_know.mp3",
-	"portal_youre_not_a_good_person.mp3",
+	"looping_radio_mix.wav","mannrobics.wav",
+	"portal_4000_degrees_kelvin.mp3","portal_still_alive.mp3",
+	"portal_you_cant_escape_you_know.mp3","portal_youre_not_a_good_person.mp3",
 
 	"ravenholm_1.mp3"
 }
 
-
 local repoURL = "https://raw.githubusercontent.com/lautaroleonel1012-commits/valve_radio/main/"
+local prefix = "valve_radio_"
 
+-------------------------------------------------
 
-for _, file in ipairs(audioFiles) do
-	local url = repoURL .. file
-
-	pcall(function()
-		writefile("valve_radio_" .. file, game:HttpGet(url))
-	end)
+local function ensureFile(file)
+	local path = prefix .. file
+	if not isfile(path) then
+		writefile(path, game:HttpGet(repoURL .. file))
+	end
+	return getcustomasset(path)
 end
 
+-------------------------------------------------
 
-local sounds = {}
-local currentSound = nil
+local sound = Instance.new("Sound")
+sound.Volume = 1
+sound.Parent = handle
 
-for _, file in ipairs(audioFiles) do
-	local sound = Instance.new("Sound")
-	sound.SoundId = getcustomasset("valve_radio_" .. file)
-	sound.Volume = 1
-	sound.Parent = handle
-	sound.Looped = false
-	table.insert(sounds, sound)
-end
-
+local currentFile = nil
 
 local function playRandom()
-	if currentSound then
-		currentSound:Stop()
+	local file = audioFiles[math.random(#audioFiles)]
+	currentFile = file
+
+	sound:Stop()
+	sound.SoundId = ensureFile(file)
+	sound:Play()
+end
+
+sound.Ended:Connect(playRandom)
+
+-------------------------------------------------
+-- BACKGROUND DOWNLOAD
+task.spawn(function()
+	for _, file in ipairs(audioFiles) do
+		ensureFile(file)
+		task.wait(0.05)
 	end
+end)
 
-	currentSound = sounds[math.random(1, #sounds)]
-	currentSound:Play()
-end
-
-for _, s in ipairs(sounds) do
-	s.Ended:Connect(playRandom)
-end
-
+-------------------------------------------------
 
 tool.Equipped:Connect(function()
 	playRandom()
 end)
 
 tool.Unequipped:Connect(function()
-	if currentSound then
-		currentSound:Stop()
-	end
+	sound:Stop()
 end)
-
 
 UserInputService.InputBegan:Connect(function(input, gpe)
 	if gpe then return end
